54e3a16e5435112e4acf331427e5eb12436028ae
Viktor Gamov <gamussa@gmail.com>
2021-11-09 01:43:07 -0500

    kuma demo on minikube works

---
+++ gateway4mesh/kuma/commands.sh
@@ -0,0 +1,33 @@
+kumactl install control-plane --license-path ~/tmp/istio+kong/license.json| kaf -
+
+kubectl port-forward svc/kong-mesh-control-plane -n kong-mesh-system 5681:5681 
+
+#open browser and show UI
+
+#deploy services
+
+kubectl create namespace boutique
+
+#kubens
+kubectl patch namespace boutique -p '{"metadata":{"annotations":{"kuma.io/sidecar-injection":"enabled"}}}'
+
+export PROJECT_ID=`gcloud config list --format 'value(core.project)'`
+skaffold run --default-repo=gcr.io/$PROJECT_ID
+
+# -> show k9s
+
+kumactl install metrics | kubectl apply -f -
+kumactl install tracing | kubectl apply -f -
+kumactl install logging | kubectl apply -f -
+
+kumactl install gateway kong | kubectl apply -f -
+kubectl apply -f ./ingress.yaml -n boutique
+kubectl plugins/02_rate-limit.yaml
+kubectl -n boutique patch ingress frontend -p '{"metadata":{"annotations":{"konghq.com/plugins":"rate-limit"}}}'
+
+helm install locust deliveryhero/locust --set loadtest.name=my-loadtest --set loadtest.locust_locustfile_configmap=my-loadtest-locustfile
+
+
+kubectl apply -f ./mesh.yaml
+
+#show gateway UI
