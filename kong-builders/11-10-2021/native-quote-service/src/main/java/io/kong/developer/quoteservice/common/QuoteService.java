7abfb8d3ac6b2ca003c7ef6d7c67c7d7b8ad43a2
Viktor Gamov <gamussa@gmail.com>
2021-11-30 14:40:42 -0500

    added knative demo

---
+++ kong-builders/11-10-2021/native-quote-service/src/main/java/io/kong/developer/quoteservice/common/QuoteService.java
@@ -0,0 +1,34 @@
+package io.kong.developer.quoteservice.common;
+
+import com.github.javafaker.Faker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class QuoteService {
+
+  @Value("${QUOTE_SERVICE:backToFuture}")
+  private String quoteConfig;
+
+  private final Faker faker;
+
+  public String getFakeQuote() {
+    final String quote;
+
+    switch (quoteConfig.toUpperCase()) {
+      case "CHUCK":
+        quote = faker.chuckNorris().fact();
+        break;
+      case "HOBBIT":
+        quote = faker.hobbit().quote();
+        break;
+      default:
+        quote = faker.backToTheFuture().quote();
+    }
+    return quote;
+  }
+}
---
+++ kong-builders/11-10-2021/native-quote-service/src/main/java/io/kong/developer/quoteservice/common/Response.java
@@ -0,0 +1,9 @@
+package io.kong.developer.quoteservice.common;
+
+import lombok.Data;
+
+@Data
+public class Response {
+
+  private final String message;
+}
