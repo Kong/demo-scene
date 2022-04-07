f4657f30e17ec3177ee46f433abe5863682a36ae
Viktor Gamov <gamussa@gmail.com>
2022-04-06 22:31:34 -0400

    enabling liveleness probe

--- quotes-service/src/main/java/io/kong/developer/servicereactive/rest/QuoteEndpoint.java
+++ quotes-service/src/main/java/io/kong/developer/servicereactive/rest/QuoteEndpoint.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.net.InetAddress;
 import java.util.Optional;
 
+import io.fabric8.kubernetes.api.model.Config;
 import io.kong.developer.servicereactive.common.QuoteService;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
@@ -24,9 +25,10 @@ class QuoteEndpoint {
   @GetMapping(value = "/", produces = {MediaType.APPLICATION_JSON_VALUE})
   public ResponseEntity<Response> hello() {
     final Response response = new Response(quoteService.getFakeQuote());
+    
     HttpHeaders httpHeaders = new HttpHeaders();
     final Optional<String> hostname = Optional.ofNullable(System.getenv("HOSTNAME"));
-    final Optional<String> podNamespace = Optional.ofNullable(System.getenv("POD_NAMESPACE"));
+    final Optional<String> podNamespace = Optional.ofNullable(System.getenv("KUBERNETES_NAMESPACE"));
     httpHeaders.add("X-Pod-Name", hostname.orElse(String.valueOf(InetAddress.getLocalHost())));
     httpHeaders.add("X-Pod-Namespace", podNamespace.orElse("UNKNOWN"));
     return new ResponseEntity<>(response, httpHeaders, HttpStatus.OK);
