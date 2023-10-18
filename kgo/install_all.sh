4bf4163be38ac8a1b841395ee4517b1b188e8241
Viktor Gamov <gamussa@gmail.com>
2023-10-18 19:10:51 -0400

    kgo 1.x demo update a fist step

--- kgo/install_all.sh
+++ kgo/install_all.sh
@@ -11,14 +11,16 @@ sleep 5
 
 echo -e "$BLUE\n=== ☸️ $BOLD Installing Gateway API CRDs $NOCOLOR $BLUE===\n$NOCOLOR"
 set -x
-kubectl kustomize "https://github.com/kubernetes-sigs/gateway-api/config/crd?ref=v0.5.1" | kubectl apply -f -
+kubectl kustomize "https://github.com/kubernetes-sigs/gateway-api/config/crd?ref=v0.8.1" | kubectl apply -f -
 set +x
 
 sleep 5
 
 echo -e "$BLUE\n=== 🦍 Installing Kong Gateway Operator $NOCOLOR $BLUE===\n$NOCOLOR"
 set -x
-kubectl kustomize "https://github.com/kong/gateway-operator-docs/config/default?submodules=false" | kubectl apply -f -
+kubectl apply -f https://docs.konghq.com/assets/gateway-operator/v1.0.0/crds.yaml --server-side
+#kubectl apply -f https://docs.konghq.com/assets/gateway-operator/v1.0.0/all_controllers.yaml
+kubectl kustomize "https://github.com/kong/gateway-operator/config/default" | kubectl apply -f -
 set +x
 
 sleep 5
