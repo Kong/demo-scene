abb29cb5f13f03f5de43f3a059b6516d2f077e46
Viktor Gamov <gamussa@gmail.com>
2023-10-20 10:44:53 -0400

    updated steps to

--- kgo/uninstall_all.sh
+++ kgo/uninstall_all.sh
@@ -4,11 +4,11 @@ set +x
 kubectl get gateways
 
 echo -e "\n=== ❌ 🦍 Uninstalling Kong Gateway Operator  ===\n"
-kubectl delete -f https://docs.konghq.com/assets/gateway-operator/v1.0.0/crds.yaml
-kubectl kustomize "https://github.com/kong/gateway-operator/config/default" | kubectl delete -f -
+kubectl apply -f https://docs.konghq.com/assets/gateway-operator/v1.0.2/all_controllers.yaml
+kubectl apply -f https://docs.konghq.com/assets/gateway-operator/v1.0.2/crds.yaml 
 
 echo -e "\n=== ❌ ☸️ Installing Gateway API CRDs ===\n"
-kubectl kustomize "https://github.com/kubernetes-sigs/gateway-api/config/crd?ref=v0.8.1" | kubectl delete -f -
+kubectl delete -k "https://github.com/kubernetes-sigs/gateway-api/config/crd?ref=v0.8.1"
 
 echo -e "\n=== ❌ 🦍 Installing Kong CRDs ===\n"
-kubectl kustomize https://github.com/Kong/kubernetes-ingress-controller/config/crd | kubectl delete -f -
+kubectl delete -k https://github.com/Kong/kubernetes-ingress-controller/config/crd
